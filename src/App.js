87b21314e1f603ac325a8efe7aca0f37a0a31adc
IvanR <ivan.ramirezf@gmail.com>
2023-12-07 06:01:07 +1000

    Controlled elements
    - Added state for 3 controlled elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("USD");
+
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="AUD">AUD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="AUD">AUD</option>
         <option value="EUR">EUR</option>
